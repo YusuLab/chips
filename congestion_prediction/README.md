7044f780703fee5a3eea4bd0bd0a06d049cbf43b
hytruongson <sonpascal93@gmail.com>
2023-02-05 09:33:43 -0800

    update

--- congestion_prediction/README.md
+++ congestion_prediction/README.md
@@ -1,1 +1,14 @@
 # Congestion prediction
+
+### Datasets
+Directory: ```data/```
+
+### Models
+Directory: ```models/```
+
+### Experiments
+Directory: ```experiments/```
+
+### Positional encodings
+Directory: ```position_enc/```
+
---
+++ congestion_prediction/experiments/test_dataset.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.optim import Adam, Adagrad
+from torch import optim
+from torch.utils.data import DataLoader
+
+import numpy as np
+import os
+import time
+import argparse
+import pickle
+
+print('Done')
