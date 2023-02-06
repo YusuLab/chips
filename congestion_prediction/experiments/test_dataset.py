e50002c50abdb931fedb868f5eb443366280d7d4
hytruongson <sonpascal93@gmail.com>
2023-02-05 19:52:23 -0800

    update

--- congestion_prediction/experiments/test_dataset.py
+++ congestion_prediction/experiments/test_dataset.py
@@ -14,10 +14,18 @@ import pickle
 # Dataset
 from RosettaStone_Dataset_PyG import RosettaStone_Dataset_PyG
 
+# Data directory
 data_dir = '../data/RosettaStone-GraphData-2023-01-21/'
 
+# Create PyG datasets
 train_dataset = RosettaStone_Dataset_PyG(data_dir = data_dir, split = 'train')
 valid_dataset = RosettaStone_Dataset_PyG(data_dir = data_dir, split = 'valid')
 test_dataset = RosettaStone_Dataset_PyG(data_dir = data_dir, split = 'test')
+
+# Create PyG data loaders
+batch_size = 1
+train_dataloader = DataLoader(train_dataset, batch_size, shuffle = True)
+valid_dataloader = DataLoader(valid_dataset, batch_size, shuffle = False)
+test_dataloader = DataLoader(test_dataset, batch_size, shuffle = False)
 
 print('Done')
