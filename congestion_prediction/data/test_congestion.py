c2ba510609bca86c65442c6494c0bc9f175186f8
hytruongson <sonpascal93@gmail.com>
2023-02-24 12:15:11 -0800

    update

--- congestion_prediction/data/test_congestion.py
+++ congestion_prediction/data/test_congestion.py
@@ -2,6 +2,8 @@ import numpy as np
 import gzip
 import json
 
+# Slow getGRCIndex
+'''
 def getGRCIndex(x, y, xbl, ybl):
     j = 0
     for b in xbl[1:]:
@@ -14,11 +16,34 @@ def getGRCIndex(x, y, xbl, ybl):
             break
         i += 1
     return i, j
+'''
+
+# Fast getGRCIndex
+def binary_search(key, array):
+    if key < array[0]:
+        return 0
+    start = 0
+    finish = len(array) - 1
+    best = 0
+    while start <= finish:
+        mid = (start + finish) // 2
+        if key >= array[mid]:
+            best = max(best, mid)
+            start = mid + 1
+        else:
+            finish = mid - 1
+    return best + 1
+
+def getGRCIndex(x, y, xbl, ybl):
+    j = binary_search(x, xbl[1:])
+    i = binary_search(y, ybl[1:])
+    return i, j
 
 # Design
 # design = 'counter'
-design = 'xbar'
-# design = 'RocketTile'
+# design = 'xbar'
+design = 'RocketTile'
+print('Design:', design)
 
 # Information about instances and nets
 instances_nets_fn = 'NCSU-DigIC-GraphData-2022-10-15/' + design + '/' + design + '.json.gz'
@@ -41,25 +66,47 @@ congestion_fn = 'NCSU-DigIC-GraphData-2022-10-15/' + design + '/' + design + '_c
 # Load congestion data file
 congestion_data = np.load(congestion_fn)
 
-lyr = list(congestion_data['layerList']).index('M1')
-ybl = congestion_data['yBoundaryList']
-xbl = congestion_data['xBoundaryList']
+num_layers = len(list(congestion_data['layerList']))
+print('Number of layers:', num_layers)
+print('Layers:', list(congestion_data['layerList']))
 
-count_congestions = 0
-for idx in range(num_instances):
-    xloc = instances[idx]['xloc']
-    yloc = instances[idx]['yloc']
-    i, j = getGRCIndex(xloc, yloc, xbl, ybl)
+for layer in list(congestion_data['layerList']):
+    print(layer, '--------------------------')
+    lyr = list(congestion_data['layerList']).index(layer)
+    ybl = congestion_data['yBoundaryList']
+    xbl = congestion_data['xBoundaryList']
 
-    demand = congestion_data['demand'][lyr][i][j]
-    capacity = congestion_data['capacity'][lyr][i][j]
+    count_congestions = 0
+    sum_diff = 0
+    sum_demand = 0
+    sum_capacity = 0
 
-    # If demand is bigger than capacity then increse the number of congestions by 1
-    if demand > capacity:
-        count_congestions += 1
+    for idx in range(num_instances):
+        xloc = instances[idx]['xloc']
+        yloc = instances[idx]['yloc']
+        i, j = getGRCIndex(xloc, yloc, xbl, ybl)
 
-    if (idx + 1) % 1000 == 0:
-        print('Done scanning for', idx + 1, 'instances')
+        demand = congestion_data['demand'][lyr][i][j]
+        capacity = congestion_data['capacity'][lyr][i][j]
 
-print('Number of congestions:', count_congestions)
+        # Statistics
+        sum_diff += (capacity - demand)
+        sum_demand += demand
+        sum_capacity += capacity
+
+        # If demand is bigger than capacity then increse the number of congestions by 1
+        if demand > capacity:
+            count_congestions += 1
+
+        if (idx + 1) % 1000 == 0:
+            print('Done scanning for', idx + 1, 'instances')
+
+    average_diff = sum_diff / num_instances
+    average_demand = sum_demand / num_instances
+    average_capacity = sum_capacity / num_instances
+    
+    print('Number of demand > capacity:', count_congestions)
+    print('Average capacity - demand:', average_diff)
+    print('Average demand:', average_demand)
+    print('Average capacity:', average_capacity)
 print('Done')
