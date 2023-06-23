09d630f2bfed03c9b8340fb067945e3644bf9f8d
hytruongson <sonpascal93@gmail.com>
2023-06-23 16:54:36 -0700

    update

--- congestion_prediction/experiments/node/train_netlistgnn.sh
+++ congestion_prediction/experiments/node/train_netlistgnn.sh
@@ -6,9 +6,6 @@ target=demand
 #target=capacity
 #target=congestion
 
-# Index of graph
-graph_index=26
-
 mkdir $program
 cd $program
 mkdir $target
@@ -17,17 +14,17 @@ dir=./$program/$target/
 
 data_dir=/data/zluo/hetero_net/data/2023-03-06_data/
 
-num_epoch=100
+num_epoch=1000
 batch_size=1
 learning_rate=0.001
 seed=123456789
 
 # For NetlistGNN
 n_layer=2
-node_feats=8 #64
-net_feats=8 #128
-pin_feats=8
-edge_feats=2
+node_feats=16 #64
+net_feats=16 #128
+pin_feats=16
+edge_feats=4
 
 # Device
 device=cuda
@@ -47,7 +44,9 @@ load_pd=1
 test_mode=0
 
 # Execution
+for graph_index in 0 5 10 16 21 26
+do
 name=${program}.${target}.n_layer.${n_layer}.node_feats.${node_feats}.net_feats.${net_feats}.pin_feats.${pin_feats}.edge_feats.${edge_feats}.num_epoch.${num_epoch}.batch_size.${batch_size}.learning_rate.${learning_rate}.seed.${seed}.pe.${pe}.pos_dim.${pos_dim}.load_global_info.${load_global_info}.load_pd.${load_pd}.graph_index.${graph_index}
 CUDA_VISIBLE_DEVICES=$device_idx python3 $program.py --n_layer=$n_layer --node_feats=$node_feats --net_feats=$net_feats --pin_feats=$pin_feats --edge_feats=$edge_feats --target=$target --dir=$dir --name=$name --num_epoch=$num_epoch --batch_size=$batch_size --learning_rate=$learning_rate --seed=$seed --pe=$pe --pos_dim=$pos_dim --load_global_info=$load_global_info --load_pd=$load_pd --test_mode=$test_mode --device=$device --data_dir=$data_dir --graph_index=$graph_index
-
+done
 
